2e5e4c6d88d96e7992b9e6645ab036ffba3dd599
Bhushan Mendhe <bhushanmendhe15@gmail.com>
2021-09-24 09:12:11 +0530

    javascript complete

--- javascript.js
+++ javascript.js
@@ -1,22 +1,29 @@
-const billAmount = document.querySelector("#bill-amount");
-const cashAmount = document.querySelector("#cash-amount");
+var billAmount = document.querySelector("#bill-amount");
+var cashAmount = document.querySelector("#cash-amount");
 const checkButton = document.querySelector("#check-button");
 const errorMessage = document.querySelector("#error-message");
+const noOfNotes = document.querySelectorAll(".no-of-notes");
 
 errorMessage.style.display = "none";
+
+const changeAvailable = [2000,500,100,20,10,5,1]; //Available Denomination
 
 checkButton.addEventListener("click", validateBillAndCashAmount);
 
 function validateBillAndCashAmount() {
-    if(billAmount.value > 0) {
-        if(cashAmount.value >= billAmount.value) {
+    billAmount = parseInt(billAmount.value); //to convert bill amount from string to int
+    cashAmount = parseInt(cashAmount.value); //to convert cash amount from string to int
+
+    if(billAmount > 0) {
+        if(cashAmount >= billAmount) {
             errorMessage.style.display = "none"; //hides eroor message
-            const amountToBeReturned =  cashAmount.value - billAmount.value;
+            const amountToBeReturned =  cashAmount - billAmount;
+            calculateChange(amountToBeReturned); //calling calculate change function
         } else {
             showErrorMessage("The cash provided should be greater than or equal to the bill amount.");
         }
     } else {
-        showErrorMessage("Invalid Bill Amount");
+        showErrorMessage("Enter a valid Bill Amount");
     }
 }
 
@@ -24,4 +31,20 @@ function showErrorMessage(message) {
     errorMessage.style.display = "block";
     errorMessage.style.color = "red";
     errorMessage.innerText = message;
+}
+
+function calculateChange (amount) {
+    var changeCount = [];
+    var i = 0;
+
+    while (i<changeAvailable.length) {
+        var change = Math.floor(amount/changeAvailable[i]);
+        if( change===0 ) {
+            noOfNotes[i].innerText = 0;
+        }else {
+            amount = amount%changeAvailable[i];
+            noOfNotes[i].innerText = change;
+        }
+        i++;
+    }
 }
