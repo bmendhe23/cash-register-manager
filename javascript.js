03e387b3614830fadacdfa105087a9e7b4eaae0e
Bhushan Mendhe <bhushanmendhe15@gmail.com>
2021-09-24 11:32:28 +0530

    styles and javascript done

--- javascript.js
+++ javascript.js
@@ -1,22 +1,34 @@
 var billAmount = document.querySelector("#bill-amount");
 var cashAmount = document.querySelector("#cash-amount");
 const checkButton = document.querySelector("#check-button");
+const checkNext = document.querySelector("#check-bill");
 const errorMessage = document.querySelector("#error-message");
 const noOfNotes = document.querySelectorAll(".no-of-notes");
 
-errorMessage.style.display = "none";
+
 
 const changeAvailable = [2000,500,100,20,10,5,1]; //Available Denomination
 
 checkButton.addEventListener("click", validateBillAndCashAmount);
+checkNext.addEventListener("click", validateBillAndCashAmount);
+
+function showElement() {
+    document.getElementById("label").hidden = false;
+    document.getElementById("cash-amount").hidden = false;
+    document.getElementById("check-button").hidden = false;
+    document.getElementById("check-bill").hidden = true;
+}
 
 function validateBillAndCashAmount() {
     billAmount = parseInt(billAmount.value); //to convert bill amount from string to int
     cashAmount = parseInt(cashAmount.value); //to convert cash amount from string to int
+    
+    errorMessage.style.display = "none";
 
     if(billAmount > 0) {
+        showElement();
         if(cashAmount >= billAmount) {
-            errorMessage.style.display = "none"; //hides eroor message
+            errorMessage.style.display = "none"; //hides error message
             const amountToBeReturned =  cashAmount - billAmount;
             calculateChange(amountToBeReturned); //calling calculate change function
         } else {
@@ -24,6 +36,7 @@ function validateBillAndCashAmount() {
         }
     } else {
         showErrorMessage("Enter a valid Bill Amount");
+
     }
 }
 
