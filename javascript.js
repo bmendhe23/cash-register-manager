e01e584c411d29fa17e23c75a65b7471628bc5ad
Bhushan Mendhe <bhushanmendhe15@gmail.com>
2021-09-24 07:28:05 +0530

    bill and cash validation

--- javascript.js
+++ javascript.js
@@ -1,8 +1,27 @@
 const billAmount = document.querySelector("#bill-amount");
-
-billAmount.addEventListener()
-
-
-
 const cashAmount = document.querySelector("#cash-amount");
+const checkButton = document.querySelector("#check-button");
+const errorMessage = document.querySelector("#error-message");
 
+errorMessage.style.display = "none";
+
+checkButton.addEventListener("click", validateBillAndCashAmount);
+
+function validateBillAndCashAmount() {
+    if(billAmount.value > 0) {
+        if(cashAmount.value >= billAmount.value) {
+            errorMessage.style.display = "none"; //hides eroor message
+            const amountToBeReturned =  cashAmount.value - billAmount.value;
+        } else {
+            showErrorMessage("The cash provided should be greater than or equal to the bill amount.");
+        }
+    } else {
+        showErrorMessage("Invalid Bill Amount");
+    }
+}
+
+function showErrorMessage(message) {
+    errorMessage.style.display = "block";
+    errorMessage.style.color = "red";
+    errorMessage.innerText = message;
+}
